77dea14622b0cbea92ecc775bc41c9a5dd19871e
dmp40 <dinapok@gmail.com>
2022-04-07 22:14:55 +0300

    Коне дня

--- app/src/main/java/com/example/voiceassistant/MainActivity.kt
+++ app/src/main/java/com/example/voiceassistant/MainActivity.kt
@@ -1,25 +1,59 @@
 package com.example.voiceassistant
 
+import android.content.ContentValues.TAG
+import android.nfc.Tag
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.SimpleAdapter
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.textfield.TextInputEditText
+
+//46EEP7-5HXKV2K3RJ
+//AppName voice_asis  https://developer.wolframalpha.com/portal/myapps/index.html
+
 
 class MainActivity : AppCompatActivity() {
+    val TAG: String = "MainActivity"
+
+    lateinit var requestInput: TextInputEditText
+    lateinit var podsAdapter: SimpleAdapter//37-40
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val TAG: String = "MainActivity"
-        val name: String = "Diman"
-        val surname: String = "Novikov"
-        var age: Int = 64
-        var heigt: Double = 180.0
 
-        val summary: String = "name: $name surname: $surname age: $age heigt: $heigt"
-         val itog: TextView = findViewById(R.id.output)
-        itog.text = summary
+        initViewc()
 
-        Log.d(TAG,summary)
+    }
+    fun  initViewc() {
+        val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toollbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+        //ур 2,5 24-31
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            when(item.itemId) {
+                R.id.action_stop -> {
+                    Log.e(TAG,"stop")
+                    return true
+                }
+                R.id.action_clear -> {
+                    Log.e(TAG,"action_clear")
+                    Log.e("tag", "tag")
+                    return true
+                }
+            }
+            return super.onOptionsItemSelected(item)
 
     }
 }
